6e257115eb33006bc90a202c07b986e2c05373e6
huangyingcan <your_email@example.com>
2024-04-04 20:30:54 +0800

    初始化项目

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1712225654823_8287',
+  koa: {
+    port: 1123,
+  },
+} as MidwayConfig;
---
+++ src/controller/get-captcha.ts
@@ -0,0 +1,34 @@
+import { Inject, Controller, Post, Body } from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
+import { CaptchaService } from '@midwayjs/captcha';
+@Controller('/captcha')
+export class APIController {
+  @Inject()
+  ctx: Context;
+
+  
+  @Inject()
+  captchaService: CaptchaService;
+
+  @Post('/getImg')
+  async getImg() {
+    const { id, imageBase64 } = await this.captchaService.image({ width: 120, height: 40 });
+    return {
+      id,          // 验证码 id
+      imageBase64, // 验证码 SVG 图片的 base64 数据，可以直接放入前端的 img 标签内
+    }
+  }
+  @Post('/checkCaptcha')
+  async checkImg(@Body('id') id:string,@Body('answer') answer:string ) {
+    if(!id || !answer){
+        return 'error requests'
+    }
+    const passed: boolean = await this.captchaService.check(id, answer);
+    if (passed) {
+      return 'passed';
+    }
+    return 'error';
+
+  }
+
+}
