bde54d5dac806e1800b471236fa0f24ed69362e5
huangyingcan <>
2025-10-20 17:25:43 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,39 @@
+// module.exports = {
+//   apps: [{
+//     name: 'dana-api',
+//     script: './bootstrap.js',
+//     instances: 1,
+//     autorestart: true,
+//     watch: false,
+//     max_memory_restart: '1G',
+//     env: {
+//       NODE_ENV: 'production',
+//       PORT: 1123
+//     },
+//     env_production: {
+//       NODE_ENV: 'production',
+//       PORT: 1123
+//     }
+//   }]
+// };
 module.exports = {
   apps: [{
     name: 'dana-api',
-    script: './bootstrap.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
+    script: 'npm',
+    args: 'run prod',
+    cwd: '/home/midway-deploy/apps/dana-api',
     env: {
       NODE_ENV: 'production',
       PORT: 1123
     },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 1123
-    }
+    error_file: '/home/midway-deploy/logs/dana-api-error.log',
+    out_file: '/home/midway-deploy/logs/dana-api-out.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    instances: 1,
+    exec_mode: 'fork'
   }]
 };
