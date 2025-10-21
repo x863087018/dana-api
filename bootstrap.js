58feeed5bf33b43f3e98d5058410a122a554b6bb
huangyingcan <>
2025-10-21 14:58:34 +0800

    问题修复

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,12 @@
 const { Bootstrap } = require('@midwayjs/bootstrap');
-Bootstrap.run();
+
+Bootstrap.configure({
+  appDir: __dirname,
+  // 如果在根目录运行，使用 dist 目录
+  baseDir: __dirname + '/dist',
+  // 如果直接在 dist 目录运行，注释掉上面一行
+  // baseDir: __dirname,
+}).run().catch((err) => {
+  console.error('Failed to start application:', err);
+  process.exit(1);
+});
