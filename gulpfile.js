a722784d945604cebe4eb8a5e2f849c406ad2c9d
huangyingcan <>
2025-10-21 16:05:26 +0800

    配置修改

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,7 @@ const shell = require('gulp-shell');
 const zip = require('gulp-zip');
 const fs = require('fs');
 
-const projectName = require('./package.json').name;
-const projectVersion = require('./package.json').version;
-
-const distProject = `dist/${projectName}`;
-const distZip = `${projectName}-${projectVersion}.zip`;
-
-gulp.task('clean', () => del(['dist/**', '!*.zip']));
+gulp.task('clean', () => del(['dist/**']));
 
 // gulp.task('lint', shell.task(['npm run lint']));
 
@@ -18,7 +12,6 @@ gulp.task('tsc', shell.task(['midway-bin build -c']));
 
 gulp.task('copy', () => {
     const fileList = [
-        'dist/**',
         'package.json'
     ];
     const packageLock = 'package-lock.json';
@@ -26,27 +19,18 @@ gulp.task('copy', () => {
         fileList.push(packageLock);
     }
     return gulp
-        .src(fileList, {
-            base: './',
-        })
-        .pipe(gulp.dest(distProject));
-});
-
-gulp.task('del-dup-dist', () => del(['dist/**', `!${distProject}`]));
-
-gulp.task(
-    'npm',
-    shell.task([`cd ${distProject} && npm install --production --no-audit`])
-);
-
-gulp.task('zip', () => {
-    return gulp
-        .src(`${distProject}/**`, { base: 'dist/' })
-        .pipe(zip(distZip))
+        .src(fileList)
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task(
-    'build',
-    gulp.series('clean', 'tsc', 'copy', 'del-dup-dist', 'npm', 'zip')
+    'npm',
+    shell.task(['cd dist && npm install --production --no-audit'])
 );
+
+gulp.task(
+    'build',
+    gulp.series('clean', 'tsc', 'copy', 'npm')
+);
+
+gulp.task('default', gulp.series('build'));
